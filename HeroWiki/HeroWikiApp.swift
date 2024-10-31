a1bab2dd70df3df10ddf3c3e46cd3584a1d34c13
AudyDev <audyDev@gmail.com>
2024-10-31 12:32:10 +0100

    Initial Commit

---
+++ HeroWiki/HeroWikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroWikiApp.swift
+//  HeroWiki
+//
+//  Created by AudyDev on 31/10/24.
+//
+
+import SwiftUI
+
+@main
+struct HeroWikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
