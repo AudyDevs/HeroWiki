9693faaaad8550d54b30fa8525c17f66870aa93f
AudyDev <audyDev@gmail.com>
2024-11-20 18:38:34 +0100

    fix tests

--- HeroWikiTests/DetailViewModelTests.swift
+++ HeroWikiTests/DetailViewModelTests.swift
@@ -6,30 +6,26 @@
 //
 
 import XCTest
+@testable import HeroWiki
 
-final class DetailViewModelTests: XCTestCase {
-
+class DetailVMTests: XCTestCase {
+    var detailViewModel: DetailVM!
+    var idModel: String!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        detailViewModel = DetailVM()
+        idModel = "1"
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        detailViewModel = nil
+        idModel = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func test_DetailViewModel_ShouldReturnACorrectSuperHeroModel() throws {
+        Task{
+            await detailViewModel.getHeroById(id: idModel)
+            XCTAssert(detailViewModel.superhero?.id == idModel)
         }
     }
-
 }
